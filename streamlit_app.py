bf58d27abeec8d838f5ca699aa623ae49a3736c2
oardilac <odardilacueto@gmail.com>
2024-10-08 19:50:28 -0500

    Refactor streamlit_app.py to invert signals and add interactive plotting

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,40 +4,89 @@ import plotly.graph_objs as go
 import streamlit as st
 import time
 
-# Datos originales
-Delta = 0.01
+Delta = 0.01    # Delta
 
-# Tiempo total
-ta = [0]
+ta=[0]
 ta2 = np.arange(0, 3, Delta)
 ta3 = np.arange(3, 5, Delta)
 ta4 = [5]
-ta_T = np.concatenate((ta, ta2, ta3, ta4))
+ta_T=np.concatenate((ta, ta2, ta3,ta4))  #Tiempo total a
 
-# Valores de la función
-a = [0]
-xa2 = 2 * np.ones(len(ta2))
-xa3 = -2 * np.ones(len(ta3))
-xa4 = [0]
-x_ta = np.concatenate((a, xa2, xa3, xa4))
+tb = [-1]
+tb2 = np.arange(-1, 1, Delta)
+tb3 = [1]
+tb_T=np.concatenate((tb, tb2, tb3))  #Tiempo total b
 
-# Invertir los valores del eje X y los valores correspondientes del eje Y
-ta_T_inverted = -ta_T[::-1]
-x_ta_inverted = x_ta[::-1]
+tc = [-1]
+tc2 = np.arange(-1, 1, Delta)
+tc3 = np.arange(1, 3, Delta)
+tc4 = np.arange(3, 5, Delta)
+tc5 = [5]
+tc_T=np.concatenate((tc, tc2, tc3,tc4,tc5))  #Tiempo total c
+
+td = [-3]
+td2 = np.arange(-3, 0, Delta)
+td3 = np.arange(0, 3, Delta)
+td4 = [3]
+td_T=np.concatenate((td, td2, td3,td4))  #Tiempo total d
+
+xa=[0]
+xa2=2*np.ones(len(ta2))
+xa3=-2*np.ones(len(ta3))
+xa4=[0]
+x_ta = np.concatenate((xa, xa2,xa3,xa4 ))   #Funcion a Continua
 
 
-def graficar(ta, x_ta):
+xb=[0]
+xb2=-tb2*1
+xb3=[0]
+x_tb = np.concatenate((xb, xb2,xb3 ))   #Funcion b Continua
+
+
+xc=[0]
+xc2=2*np.ones(len(tc2))
+xc3=-2*tc3+4
+xc4=-2*np.ones(len(tc4))
+xc5=[0]
+x_tc = np.concatenate((xc, xc2,xc3,xc4,xc5 ))   #Funcion c Continua
+
+xd=[0]
+xd2=np.exp(td2)
+xd3=np.exp(-td3)
+xd4=[0]
+x_td = np.concatenate((xd, xd2,xd3,xd4 ))   #Funcion d Continua
+
+def invertir(t, x_t):
+    return -t[::-1], x_t[::-1]
+
+def grafica_continua(t, x_t, color, title):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=t, y=x_t, mode="lines", name=title, line=dict(color=color)))
+
+    fig.update_layout(
+        xaxis_title="Tiempo",
+        yaxis_title="Amplitud",
+        showlegend=True,
+    )
+
+    fig.update_xaxes(showgrid=True)
+    fig.update_yaxes(showgrid=True)
+
+    # Mostrar gráfico en la primera columna
+    st.plotly_chart(fig, use_container_width=True)
+
+def graficar(t, x_t):
     plot_placeholder = st.empty()
 
     # Crear la figura inicial
-    trace = go.Scatter(x=ta, y=x_ta, mode='lines', name='Señal Invertida')
+    trace = go.Scatter(x=t, y=x_t, mode='lines', name='Señal Invertida')
     layout = go.Layout(xaxis=dict(range=[-21, 21]), yaxis=dict(range=[-3, 3]), title='Señal Invertida en Movimiento')
     fig = go.Figure(data=[trace], layout=layout)
 
     # Actualizar la señal para moverla de izquierda a derecha en el eje X
     for shift in np.linspace(-20, 20, 20):
         # Actualizar los valores de X para moverlos
-        new_x = ta_T_inverted + shift
+        new_x = t + shift
         fig.data[0].x = new_x
         
         # Renderizar la figura actualizada en el mismo lienzo
@@ -48,7 +97,7 @@ def graficar(ta, x_ta):
 
 
 st.set_page_config(layout="wide")
-st.title("Interfaz gráfica de procesamiento de señales")
+st.title("Interfaz gráfica de convolución de señales")
 
 st.sidebar.title("Menu de operaciones")
 operation = st.sidebar.selectbox(
@@ -56,15 +105,6 @@ operation = st.sidebar.selectbox(
 )
 
 if operation == "Menú Inicial...":
-    st.markdown("Este laboratorio tiene como objetivo poner en práctica los conceptos teóricos aprendidos en el curso de Señales y Sistemas. Aquí tendrás la oportunidad de realizar operaciones fundamentales de transformación de señales y visualizar los resultados en un entorno gráfico computacional.")
-    st.markdown("Para comenzar a usar la aplicación, dirígete a la parte izquierda y selecciona el menú desplegable debajo del menú de operaciones. Desde allí podrás escoger el tipo de señal con la que deseas trabajar y también volver a esta pestaña cuando lo desees.")
-    st.subheader("Explicación Teorica de las transformaciones")
-    st.markdown("La transformación de señales se refiere a los cambios que pueden producirse en los parámetros que componen una señal. Hay dos tipos principales de transformaciones: el escalamiento y el desplazamiento. Es importante analizar por separado las transformaciones en tiempo continuo y en tiempo discreto debido a las diferencias significativas entre ambos casos.")
-    st.markdown("##### Desplazamiento en el tiempo #####")
-    st.markdown(r"Una señal $x(t)$ se desplaza en el tiempo cuando la variable $t$ en dicha señal se suma o se resta por un valor $t_0$. El desplazamiento es hacia la derecha, o un retardo, si es $-t_0$, y hacia la izquierda, o un adelanto, si es $+t_0$. Si la señal experimenta una reflexión, es decir, cuando la variable t se multiplica por menos uno $x(-t)$, los desplazamientos ocurren en la dirección opuesta.")
-    st.markdown("##### Escalamiento en el tiempo #####")
-    st.markdown(r"Una señal $x(t)$ se escala en el tiempo cuando la variable t es multiplicada por un valor absoluto $|a|$ mayor o menor que uno, es decir, $x(at)$. Si $|a|$ es mayor que 1, la señal se comprime por ese factor; si es menor que 1, se expande por el mismo factor. Cada valor de $t$ se multiplica por el inverso de $a$ para obtener los nuevos valores.")
-    st.markdown(r"Hay dos formas de transformar una señal $x(t)$ en el tiempo: primero desplazarla y luego escalarla, o realizar el proceso en el orden inverso.")
     st.sidebar.markdown("Creado por: Oliver Ardila, Juan Bermejo y Daniel Henriquez")
 
     st.markdown("#### Agradecimientos ####")
@@ -76,5 +116,51 @@ if operation == "Menú Inicial...":
 
     st.markdown("Damos gracias a Cristiano Ronaldo y a Lena Gray por el desarrollo de esta interfaz gráfica.")
 elif operation == "Continua":
-    signal = st.sidebar.radio("Señal", ["1", "2"])
-    graficar(ta_T_inverted, x_ta_inverted)
+    x_t = st.sidebar.selectbox("Señal x(t)", ["Seleccione", "A", "B", "C", "D"])
+    h_t = st.sidebar.selectbox("Señal h(t)", ["Seleccione", "A", "B", "C", "D"])
+
+    if x_t == "Seleccione" or h_t == "Seleccione":
+        st.error("Seleccione las señales a graficar")
+    else:
+        if x_t == "A":
+            x = ta_T
+            y = x_ta
+        elif x_t == "B":
+            x = tb_T
+            y = x_tb
+        elif x_t == "C":
+            x = tc_T
+            y = x_tc
+        elif x_t == "D":
+            x = td_T
+            y = x_td
+
+        if h_t == "A":
+            h = ta_T
+            z = x_ta
+        elif h_t == "B":
+            h = tb_T
+            z = x_tb
+        elif h_t == "C":
+            h = tc_T
+            z = x_tc
+        elif h_t == "D":
+            h = td_T
+            z = x_td
+
+        inv = st.sidebar.selectbox("Cual señal desea invertir", ["Seleccione", "x(t)", "h(t)"])
+        col1, col2 = st.columns(2)
+        with col1:
+            grafica_continua(x, y, "blue", "x(t)")
+        with col2:
+            grafica_continua(h, z, "red", "h(t)")
+
+        if inv == "Seleccione":
+            st.error("Seleccione la señal a invertir")
+        else:
+            if inv == "x(t)":
+                t_xinv, x_inv = invertir(x, y)
+                grafica_continua(t_xinv, x_inv, "green", "x(t) invertida")
+            elif inv == "h(t)":
+                t_hinv, h_inv = invertir(h, z)
+                grafica_continua(t_hinv, h_inv, "green", "h(t) invertida")
