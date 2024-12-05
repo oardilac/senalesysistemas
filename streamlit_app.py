0ad83de745eb90016cb9c09bd48ceaf978705dc1
oardilac <odardilacueto@gmail.com>
2024-12-05 16:10:18 -0500

    Refactor streamlit_app.py to add Fourier analysis and visualization

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,10 @@ with col4:
 with col5:
     st.page_link("pages/cuarto.py", label="DSB-LC", use_container_width=True)
 
+st.markdown("Las herramientas más esenciales en el análisis de señales y sistemas incluyen la serie y la transformada de Fourier. Su capacidad para descomponer señales en componentes sinusoidales ha sido clave en su aplicación en múltiples ámbitos. Estos conceptos son fundamentales en áreas como las telecomunicaciones, donde se utilizan para procesos como la modulación y demodulación en términos de amplitud o frecuencia.")
+st.markdown("En el tratamiento de señales, tanto la serie como la transformada de Fourier tienen un rol crucial en el diseño de filtros, permitiendo seleccionar o eliminar ciertas frecuencias en función de los requerimientos específicos de un sistema. Asimismo, en el campo del audio y sonido, estas herramientas resultan indispensables para tareas como la grabación, compresión y reproducción, además de optimizar el diseño de los circuitos asociados.")
+st.markdown("Por otro lado, el algoritmo Fast Fourier Transform (FFT) permite una implementación eficiente de la transformada discreta en distintas áreas. La FFT no solo facilita la ejecución de análisis que de otro modo serían más complejos, sino que también mejora significativamente la eficiencia en el cálculo de la transformada, especialmente en el manejo de señales de gran tamaño.")
+
 st.sidebar.markdown("Creado por: Oliver Ardila, Juan Bermejo y Daniel Henriquez")
 
 st.markdown("#### Agradecimientos ####")
